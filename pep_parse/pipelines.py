a83a447bb60544176812276e65f4b1db48b9ab20
Natalya Shulgina <natulishka84@gmail.com>
2023-05-06 22:36:35 +0300

    after 1 review

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,3 +1,4 @@
+import csv
 from datetime import datetime
 
 from scrapy.exceptions import DropItem
@@ -10,29 +11,36 @@ class PepParsePipeline:
 
     def open_spider(self, spider):
         '''Операции, вполняемые при старте работы паука.'''
-        results_dir = BASE_DIR / 'results'
-        results_dir.mkdir(exist_ok=True)
-        now = datetime.now()
-        now_formatted = now.strftime(DATETIME_FORMAT)
-        file_name = f'status_summary_{now_formatted}.csv'
-        self.file_path = results_dir / file_name
         self.amount_statuses = {}
 
     def process_item(self, item, spider):
         '''Обрабатывает объекты Items.'''
-        self.amount_statuses[item['status']] = self.amount_statuses.get(
-            item['status'], 0) + 1
         if item['status'] not in EXPECTED_STATUS:
             raise DropItem(f'''
             Обнаружен неизвестный статус у PEP № {item['number']}:
             {item['status']}
             ''')
+        self.amount_statuses[item['status']] = self.amount_statuses.get(
+            item['status'], 0) + 1
         return item
 
     def close_spider(self, spider):
         '''Операции, вполняемые при окончании работы паука.'''
-        with open(self.file_path, mode='w', encoding='utf-8') as f:
-            f.write('Статус,Количество\n')
+        results_dir = BASE_DIR / 'results'
+        results_dir.mkdir(exist_ok=True)
+        now = datetime.now()
+        now_formatted = now.strftime(DATETIME_FORMAT)
+        file_name = f'status_summary_{now_formatted}.csv'
+        file_path = results_dir / file_name
+        with open(file_path, 'w', encoding='utf-8', newline='') as csvfile:
+            summarywriter = csv.writer(csvfile)
+            summarywriter.writerow(['Статус', 'Количество'])
             for status, amount in sorted(self.amount_statuses.items()):
-                f.write(f'{status},{amount}\n')
-            f.write(f'Total,{sum(self.amount_statuses.values())}')
+                summarywriter.writerow([status, amount])
+            summarywriter.writerow(['Total',
+                                    sum(self.amount_statuses.values())])
+        # with open(file_path, mode='w', encoding='utf-8') as f:
+        #     f.write('Статус,Количество\n')
+        #     for status, amount in sorted(self.amount_statuses.items()):
+        #         f.write(f'{status},{amount}\n')
+        #     f.write(f'Total,{sum(self.amount_statuses.values())}')
